bb5e9dbf4d7cd6d26a02c09afc0fd444c04be462
YMelon <929354023@qq.com>
2018-07-28 20:51:04 +0800

    error handle for functions called in 1st way

--- sys_program/socket_test/socke_tcp/server.c
+++ sys_program/socket_test/socke_tcp/server.c
@@ -1,5 +1,6 @@
 //服务器示例 功能:小写转大写
 //突出逻辑 省略各调用函数返回值检查
+//3.加入出错处理
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,23 +20,38 @@ int main() {
     char buf[BUFSIZ];
     char cli_ip[BUFSIZ]; //用于存储连接上的客户端IP
     int n, i;
+    int ret;
 
     //创建
     sfd = socket(AF_INET, SOCK_STREAM, 0);
-
+    if(sfd == -1) {
+	perror("socket error");
+	exit(1);
+    }
     //绑定
     bzero(&serv_addr, sizeof(serv_addr)); //好习惯 清零 对比memset
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERV_PORT);
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(sfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
-    //设置同时可连接数
-    listen(sfd, 128);//默认上限
+    ret =  bind(sfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if(ret == -1) {
+	perror("bind error");
+	exit(1);
+    }
+    //设置同时可连接数 好好理解这里
+    ret = listen(sfd, 128);//默认上限
+    if(ret == -1) {
+	perror("listen error");
+	exit(1);
+    }
 
     //等待连接
     cli_addr_len = sizeof(cli_addr);
-    cfd = accept(sfd, (struct sockaddr*)&cli_addr,&cli_addr_len);
+    cfd = accept(sfd, (struct sockaddr*)&cli_addr, &cli_addr_len);
+    if(cfd == -1) {
+	perror("accept error");
+	exit(1);
+    }
     
     //2.打印连接好的客户端信息
     printf("client IP: %s, client port: %d\n",
